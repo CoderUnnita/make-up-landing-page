67cb6beaf8c9d0b983c11bb1e2206a00aed7af66
UnnitaY <unnitayaduvanshi@gmail.com>
2024-10-20 12:26:16 +0530

    practice

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 console.log("Hello Let's Learn JavaScript");
 const age=19;
 console.log("Age is", age);
+let arr=[12,14,16,18,20];
+console.log(arr[1]); 
+const arr1=[12,14,16,18,20];
+console.log(arr[0]); 
